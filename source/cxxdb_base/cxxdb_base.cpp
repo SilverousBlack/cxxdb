75e9acc560a9b8a5999161d7762c8b904c1b4f66
SilverousBlack <juliancalebsegundo@gmail.com>
2019-10-07 21:08:40 +0800

    0.0.0

---
+++ source/cxxdb_base/cxxdb_base.cpp
@@ -0,0 +1,5 @@
+// cxxdb_base.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
---
+++ source/cxxdb_base/cxxdb_base.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef _CXXDB_UTILITIES_HPP_
+#define _CXXDB_UTILITIES_HPP_
+#include "pch.h"
+
+namespace cxxdb {
+
+}
+
+#endif // !_CXXDB_UTILITIES_HPP_
