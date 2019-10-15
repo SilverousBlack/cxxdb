85faf948748b2560aff5789176c653d2c147248c
Silverous Black <juliancalebsegundo@gmail.com>
2019-10-15 23:49:09 +0800

    0.0.0

--- source/cxxdb_cell/cxxdb_cell.hpp
+++ source/cxxdb_cell/cxxdb_cell.hpp
@@ -22,9 +22,17 @@ namespace cxxdb {
 	/* string comparator */
 	template<typename charT>
 	static bool (*comparator)(std_string<charT>, std_string<charT>) = 0;
+	template<>
+	static bool (*comparator<char>)(std_string<char>, std_string<char>) = 0;
+	template<>
+	static bool (*comparator<wchar_t>)(std_string<wchar_t>, std_string<wchar_t>) = 0;
 	template<typename charT>
 	inline void set_comparator_common() {
 		comparator<charT> = common_str_comparator<charT>;
+	}
+	template<typename charT>
+	inline void set_comparator(bool (*newComparator)(std_string<charT>, std_string<charT>)) {
+		comparator<charT> = newComparator<charT>(std_string<charT>, std_string<charT>);
 	}
 	template<typename charT>
 	inline bool call_comparator(std_string<charT> lstr, std_string<charT> rstr) {
@@ -34,27 +42,40 @@ namespace cxxdb {
 
 	/* string content_type identifier */
 	template<typename charT>
-	static content_type (*identifier)(std_string<charT>) = 0;
+	static content_type (*identifier)(std_string<charT>) = NULL;
 	template<typename charT>
-	inline content_type common_content_identifier(std_string<charT>) {
-		return content_type(0);
-	}
+	inline content_type common_content_identifier(std_string<charT>);
 	template<typename charT>
 	inline void set_identifier_common() {
 		identifier<charT> = common_content_identifier<charT>;
 	}
 	template<typename charT>
-	inline content_type call_identifier(std_string<charT>) {
-		return content_type();
+	inline content_type call_identifier(std_string<charT> str) {
+		return content_type(identifier<charT>(str));
 	}
+
+	/* string interpreter */
+	template<typename charT>
+	static std_string<charT> (*interpreter)(std_string<charT>) = NULL;
+	
 
 	template<typename charT, const cell_mode cellMode>
 	class basic_cell {
 	private:
 		std_string<charT>	content;
 		content_type		contentType;
+		bool				isProc;
 	public:
+		basic_cell();
+		basic_cell(std_string<charT>);
+		~basic_cell();
 
+		virtual void update_content(std_string<charT>);
+		virtual void update_type(content_type);
+		
+		virtual void proc_cell();
+		virtual void unproc_cell();
+		virtual void do_proc();
 	};
 
 	template<typename charT>
@@ -79,3 +100,4 @@ namespace cxxdb {
 }
 
 #endif // !_CXXDB_CELL_HPP_
+
