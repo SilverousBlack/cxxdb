467c33c21e1149a63ad863ea5c478ef20efa46d4
Silverous Black <juliancalebsegundo@gmail.com>
2019-10-22 01:16:31 +0800

    0.0.0

--- source/cxxdb_base/cxxdb_base.hpp
+++ source/cxxdb_base/cxxdb_base.hpp
@@ -56,6 +56,71 @@ namespace cxxdb {
 		return left == right;
 	}
 
+
+	/* string content type */
+	namespace content_type {
+		enum content_type {
+			str,
+			num,
+			cur,
+			bin,
+			oct,
+			hex,
+			dat,
+		};
+	}
+
+	using content_t = content_type::content_type;
+	const content_t content_str = content_type::str;
+	const content_t content_num = content_type::num;
+	const content_t content_cur = content_type::bin;
+	const content_t content_oct = content_type::oct;
+	const content_t content_hex = content_type::hex;
+	const content_t content_dat = content_type::dat;
+
+	/* string encoding */
+	namespace encoding {
+		enum encoding {
+			ANSI,
+			UTF8,
+			UTF16,
+			UTF32,
+		};
+	}
+
+	using encoding_t = encoding::encoding;
+	const encoding_t encoding_ansi = encoding::ANSI;
+	const encoding_t encoding_utf8 = encoding::UTF8;
+	const encoding_t encoding_utf16 = encoding::UTF16;
+	const encoding_t encoding_utf32 = encoding::UTF32;
+	
+	/* interpreter object */
+	/* class decleration */
+	template<typename charT, encoding_t encodingT = encoding_ansi>
+	class interpreter {
+	private:
+		std_string<charT>	intern;
+		std_string<charT>	content;
+		long long double	val;
+		
+	public:
+		interpreter(std_string<charT>);
+		interpreter(const charT*);
+		~interpreter();
+	protected:
+		bool util_isdatfunc(std_string<charT>);
+		bool util_isstr(std_string<charT>);
+
+	};
+
+	/* class implemetation */
+	template<typename charT, encoding_t encodingT>
+	inline interpreter<charT, encodingT>::interpreter(std_string<charT> str)
+		: intern(str)
+	{
+		size_t sz = str.size();
+		if(s)
+	}
 }
 
 #endif // !CXXDB_BASE_HPP
--- source/cxxdb_cell/cxxdb_cell.hpp
+++ source/cxxdb_cell/cxxdb_cell.hpp
@@ -6,98 +6,22 @@
 #define CXXDB_CELL_HPP
 
 namespace cxxdb {
-	enum cell_mode {
-		header,
-		general,
-		function
-	};
-
-	enum content_type {
-		numerical,
-		string,
-		func,
-		gen
-	};
-
-	/* string comparator */
-	template<typename charT>
-	static bool (*comparator)(std_string<charT>, std_string<charT>) = 0;
-	template<>
-	static bool (*comparator<char>)(std_string<char>, std_string<char>) = 0;
-	template<>
-	static bool (*comparator<wchar_t>)(std_string<wchar_t>, std_string<wchar_t>) = 0;
-	template<typename charT>
-	inline void set_comparator_common() {
-		comparator<charT> = common_str_comparator<charT>;
-	}
-	template<typename charT>
-	inline void set_comparator(bool (*newComparator)(std_string<charT>, std_string<charT>)) {
-		comparator<charT> = newComparator<charT>(std_string<charT>, std_string<charT>);
-	}
-	template<typename charT>
-	inline bool call_comparator(std_string<charT> lstr, std_string<charT> rstr) {
-		if (comparator<charT> != NULL) return (*comparator<charT>)(lstr, rstr);
-		else return false;
+	namespace cell_type {
+		enum cell_type {
+			header,
+			body
+		};
 	}
 
-	/* string content_type identifier */
 	template<typename charT>
-	static content_type (*identifier)(std_string<charT>) = NULL;
-	template<typename charT>
-	inline content_type common_content_identifier(std_string<charT>);
-	template<typename charT>
-	inline void set_identifier_common() {
-		identifier<charT> = common_content_identifier<charT>;
-	}
-	template<typename charT>
-	inline content_type call_identifier(std_string<charT> str) {
-		return content_type(identifier<charT>(str));
-	}
+	static bool (*comparator)(std_string<charT>, std_string<charT>) = NULL;
 
-	/* string interpreter */
-	template<typename charT>
-	static std_string<charT> (*interpreter)(std_string<charT>) = NULL;
-	
-
-	template<typename charT, const cell_mode cellMode>
+	template<typename charT, const cell_type::cell_type cellT>
 	class basic_cell {
 	private:
-		std_string<charT>	content;
-		content_type		contentType;
-		bool				isProc;
-	public:
-		basic_cell();
-		basic_cell(std_string<charT>);
-		~basic_cell();
-
-		virtual void update_content(std_string<charT>);
-		virtual void update_type(content_type);
-		
-		virtual void proc_cell();
-		virtual void unproc_cell();
-		virtual void do_proc();
+		std_string<charT>			content;
+		std_string<charT>			intern;
+		content_type::content_type	intype;
 	};
-
-	template<typename charT>
-	class basic_cell_header : public basic_cell<charT, header> {
-	private:
-
-	public:
-
-	};
-
-	template<typename charT>
-	class basic_cell_general : public basic_cell<charT, general> {
-	private:
-		basic_cell_header<charT>* header;
-
-	public:
-
-	};
-
-	template<typename charT>
-	class basic_cell_function : public basic_cell<charT, function> {};
 }
-
 #endif // !_CXXDB_CELL_HPP_
-
